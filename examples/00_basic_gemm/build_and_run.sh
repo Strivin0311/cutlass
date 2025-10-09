3bac8afb3a9673564e2c44ad2e9dc509c8e7d844
Strivin0311 <hyp@smail.nju.edu.cn>
2025-10-09 02:53:18 +0000

    enhanced build_and_run script with skipping flags

--- examples/00_basic_gemm/build_and_run.sh
+++ examples/00_basic_gemm/build_and_run.sh
@@ -1,45 +1,90 @@
 
+#!/bin/bash
+
 SEP="--------------------------------------------------------------------"
 
-BUILD_ROOT=./build
+BUILD_ROOT=build
 SRC_ROOT=.
 COMMON_INCLUDE=../common
 
 TARGET=basic_gemm
 
+# default not skip any step except profiling
+SKIP_BUILD=false
+SKIP_RUN=false
+SKIP_PROFILE=true
 
-echo "$SEP"
-echo "Building ${TARGET}"
-echo "$SEP"
+# parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --skip-build)
+            SKIP_BUILD=true
+            ;;
+        --skip-run)
+            SKIP_RUN=true
+            ;;
+        --skip-profile)
+            SKIP_PROFILE=true
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
 
-mkdir -p $BUILD_ROOT
+# build
+if [ "$SKIP_BUILD" = false ]; then
+    echo "$SEP"
+    echo "Building ${TARGET}"
+    echo "$SEP"
 
-NVCC_GENCODE="arch=compute_90,code=sm_90"
+    rm -rf $BUILD_ROOT && mkdir -p $BUILD_ROOT || exit
 
-nvcc -ccbin g++ -gencode=$NVCC_GENCODE \
--lcuda -lcudart \
--I${COMMON_INCLUDE} \
--o $BUILD_ROOT/$TARGET $SRC_ROOT/$TARGET.cu
+    NVCC_GENCODE="arch=compute_90,code=sm_90"
 
+    nvcc -ccbin g++ -gencode=$NVCC_GENCODE \
+    -lcuda -lcudart \
+    -I${COMMON_INCLUDE} \
+    -o $BUILD_ROOT/$TARGET $SRC_ROOT/$TARGET.cu
+else
+    echo "$SEP"
+    echo "Skipping build process"
+    echo "$SEP"
+fi
 
-echo "$SEP"
-echo "Running ${TARGET}"
-echo "$SEP"
+# run
 
 CMD=$BUILD_ROOT/$TARGET
 
-$CMD
-
-
-echo "$SEP"
-echo "Profiling ${TARGET}"
-echo "$SEP"
-
-nsys profile \
-    --force-overwrite true \
-    -o ${TARGET}.nsys-rep \
-    --capture-range=cudaProfilerApi \
+if [ "$SKIP_RUN" = false ]; then
+    echo "$SEP"
+    echo "Running ${TARGET}"
+    echo "$SEP"
     $CMD
+else
+    echo "$SEP"
+    echo "Skipping run process"
+    echo "$SEP"
+fi
+
+# profile
+if [ "$SKIP_PROFILE" = false ]; then
+    echo "$SEP"
+    echo "Profiling ${TARGET}"
+    echo "$SEP"
+
+    nsys profile \
+        --force-overwrite true \
+        -o ${TARGET}.nsys-rep \
+        --capture-range=cudaProfilerApi \
+        $CMD
+else
+    echo "$SEP"
+    echo "Skipping profiling process"
+    echo "$SEP"
+fi
 
 
 echo "$SEP"
