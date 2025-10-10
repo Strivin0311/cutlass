dbaa2192244fef223d7f10c42b81d2a8a21776b1
Strivin0311 <hyp@smail.nju.edu.cn>
2025-10-10 12:59:53 +0000

    updated cmake_all script

--- cmake_all.sh
+++ cmake_all.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 BUILD_ROOT=build
-ARCH=90
+ARCH=90a # NOTE: don't forget 'a' at the end, according to https://github.com/NVIDIA/cutlass?tab=readme-ov-file#target-architecture
+
+OPTIONS=""
+# OPTIONS="-DCUTLASS_LIBRARY_KERNELS=all"
 
 rm -rf $BUILD_ROOT && mkdir -p $BUILD_ROOT && cd $BUILD_ROOT
 
-cmake .. -DCUTLASS_NVCC_ARCHS=$ARCH
+cmake .. -DCUTLASS_NVCC_ARCHS=$ARCH $OPTIONS
