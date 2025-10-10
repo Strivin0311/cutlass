fc07daad91ac7363db774d9965c099e285468b6d
Strivin0311 <hyp@smail.nju.edu.cn>
2025-10-10 09:22:33 +0000

    updated gitignore and dsl_install script

--- dsl_install.sh
+++ dsl_install.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
+# NOTE: the old way to install python interface:
+# pip install nvidia-cutlass
+# then you can have three submodules to import individually:
+# import cutlass_cppgen
+# import cutlass_library
+# import pycute
+
+# the new way to install python interface:
 pip install nvidia-cutlass-dsl
+# then you can directly import:
+# import cutlass
