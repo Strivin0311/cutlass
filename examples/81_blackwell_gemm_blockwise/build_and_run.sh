69170c6e92dd2963a3c3823ab312ec360d35d427
Strivin0311 <hyp@smail.nju.edu>
2025-11-21 02:33:20 +0000

    run through example 81

---
+++ examples/81_blackwell_gemm_blockwise/build_and_run.sh
@@ -0,0 +1,94 @@
+
+#!/bin/bash
+
+# NOTE: you should run `cmake_all.sh` in the root directory first before running this script
+
+SEP="--------------------------------------------------------------------"
+
+BUILD_ROOT=../../build
+SRC_ROOT=examples/81_blackwell_gemm_blockwise
+
+BUILD_TARGET=81_blackwell_gemm_blockwise_all
+
+RUN_TARGET=81_blackwell_gemm_blockwise
+# RUN_TARGET=81_blackwell_gemm_groupwise
+# RUN_TARGET=81_blackwell_grouped_gemm_blockwise
+# RUN_TARGET=81_blackwell_grouped_gemm_groupwise
+
+# default not skip any step except profiling
+SKIP_BUILD=false
+SKIP_RUN=false
+SKIP_PROFILE=true
+
+# parse command line arguments
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --skip-build)
+            SKIP_BUILD=true
+            ;;
+        --skip-run)
+            SKIP_RUN=true
+            ;;
+        --skip-profile)
+            SKIP_PROFILE=true
+            ;;
+        *)
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# build
+if [ "$SKIP_BUILD" = false ]; then
+    echo "$SEP"
+    echo "Building ${BUILD_TARGET}"
+    echo "$SEP"
+
+    cd $BUILD_ROOT || exit
+    make $BUILD_TARGET || exit
+    cd -
+else
+    echo "$SEP"
+    echo "Skipping build process"
+    echo "$SEP"
+fi
+
+# run
+
+CMD=$BUILD_ROOT/$SRC_ROOT/$RUN_TARGET
+
+if [ "$SKIP_RUN" = false ]; then
+    echo "$SEP"
+    echo "Running ${RUN_TARGET}"
+    echo "$SEP"
+    $CMD
+else
+    echo "$SEP"
+    echo "Skipping run process"
+    echo "$SEP"
+fi
+
+# profile
+if [ "$SKIP_PROFILE" = false ]; then
+    echo "$SEP"
+    echo "Profiling ${RUN_TARGET}"
+    echo "$SEP"
+
+    nsys profile \
+        --force-overwrite true \
+        -o ${RUN_TARGET}.nsys-rep \
+        --capture-range=cudaProfilerApi \
+        $CMD
+else
+    echo "$SEP"
+    echo "Skipping profiling process"
+    echo "$SEP"
+fi
+
+
+echo "$SEP"
+echo "Done"
+echo "$SEP"
+
