1c0b9c5a28b72759a171399288583806287356b2
Strivin0311 <hyp@smail.nju.edu>
2025-11-21 04:32:22 +0000

    updated cmake_all script for sm100 and google test dir

--- cmake_all.sh
+++ cmake_all.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 BUILD_ROOT=build
-ARCH=90a # NOTE: don't forget 'a' at the end, according to https://github.com/NVIDIA/cutlass?tab=readme-ov-file#target-architecture
+
+# NOTE: don't forget 'a' at the end, according to https://github.com/NVIDIA/cutlass?tab=readme-ov-file#target-architecture
+ARCH=90a
+# ARCH=100a 
 
 OPTIONS=""
 # OPTIONS="-DCUTLASS_LIBRARY_KERNELS=all"
+# OPTIONS="-DGOOGLETEST_DIR=/path/to/googletest"
 
 rm -rf $BUILD_ROOT && mkdir -p $BUILD_ROOT && cd $BUILD_ROOT
 
-cmake .. -DCUTLASS_NVCC_ARCHS=$ARCH $OPTIONS
+cmake --debug-output .. -DCUTLASS_NVCC_ARCHS=$ARCH $OPTIONS
